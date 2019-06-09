1689c47e2c79da1220df585591b0d85083a0b457
walteng <ngetichwalt49@gmail.com>
2019-06-09 23:48:13 +0300

    changed the html page to added classes to style in the css and make elements interactive in the js file for the portfolio and contacts sections

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,73 @@
-
-$(".imag").click(function(){
+$(".imag").click(function() {
   $(".parag").toggle();
 });
 
-$(".img").click(function(){
+$(".img").click(function() {
   $(".parg").toggle();
 });
 
-$(".im").click(function(){
+$(".im").click(function() {
   $(".par").toggle();
 });
+$('#work4').mouseenter(function() {
+  $('.work4').show();
+}).mouseleave(function() {
+  $('.work4').hide();
+});
+$('#work3').mouseenter(function() {
+  $('.work3').show();
+}).mouseleave(function() {
+  $('.work3').hide();
+});
+
+$('#work2').mouseenter(function() {
+  $('.work2').show();
+}).mouseleave(function() {
+  $('.work2').hide();
+});
+
+$('#work1').mouseenter(function() {
+  $('.work1').show();
+}).mouseleave(function() {
+  $('.work1').hide();
+});
+
+$('#work5').mouseenter(function() {
+  $('.work5').show();
+}).mouseleave(function() {
+  $('.work5').hide();
+});
+
+$('#work6').mouseenter(function() {
+  $('.work6').show();
+}).mouseleave(function() {
+  $('.work6').hide();
+});
+$('#work7').mouseenter(function() {
+  $('.work7').show();
+}).mouseleave(function() {
+  $('.work7').hide();
+});
+
+$('#work8').mouseenter(function() {
+  $('.work8').show();
+}).mouseleave(function() {
+  $('.work8').hide();
+});
+$('#contact').submit(function(e) {
+  // prevent empty fields
+    e.preventDefault();
+    $(".mail, .name").each(function(){
+        if($.trim(this.value) == ""){
+            alert('you did not fill out one of the fields');
+        } else {
+            // Submit
+            $("button").click(function(e) {
+              var name = $(".name").val();
+              return alert(name + " we have received your message. Thank you for reaching out to us.");
+              e.preventDefault();
+            });
+
+        }
+    })
+})
